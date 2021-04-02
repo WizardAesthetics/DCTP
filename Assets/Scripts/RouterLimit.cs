e90abaa0f54817cb7d144ffebe68560efd340908
JStoneEMU <78188057+JStoneEMU@users.noreply.github.com>
2021-04-02 12:29:13 -0400

    BleebleBorf

--- Assets/Scripts/RouterLimit.cs
+++ Assets/Scripts/RouterLimit.cs
@@ -9,7 +9,7 @@ public class RouterLimit : MonoBehaviour
 {
     public TextMeshProUGUI countText;
 
-    public static int count = 4;
+    public static int count2 = 4;
 
     public GameObject button;
 
@@ -23,40 +23,41 @@ public class RouterLimit : MonoBehaviour
   
     public void SetCountText()
     {
-      countText.text = count.ToString();
+       countText.text = "Sector Acess Point Limit: " + count2;
     }
 
 
     // Update is called once per frame
     public void Update()
     {
-        if (count < 1)
+        if (count2 < 1)
         {
             button.SetActive(false);
             image.SetActive(true);
         }
-        if (count >= 1)
+        if (count2 >= 1)
         {
           button.SetActive(true);
           image.SetActive(false);
         }
+        countText.text = "Sector Acess Point Limit: " + count2;
+
     }
 
     public void Reduce()
     {
-
-        count = count - 1;
-
+        count2 = count2 - 1;
+        SetCountText();
     }
 
     public int getCount()
     {
-        return count;
+        return count2;
     }
 
     public void setCount(int x)
     {
-        count = count + x;
+        count2 = count2 + x;
     }
 
 }
